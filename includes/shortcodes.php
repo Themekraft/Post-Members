ea0a88d5d6c51fe0f99863d85e48e978c95fabbb
peter huck <huck@huck-fresow.de>
2017-01-12 09:33:42 +0100

    setup ausgabe

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -57,31 +57,63 @@ function tk_pm_get_list_members( $atts = array() ) {
 		$post_id = $post->ID;
 	}
 
+
+
 	$post_members = get_post_meta( $post_id, '_tk_post_members', true );
 	$tmp = '';
 	if ( isset( $post_members ) && is_array( $post_members ) ) {
 		ob_start(); ?>
-		<ul>
+		<ul class="tk-post-member-list">
 			<?php foreach ( $post_members as $member ) {
-				$user_data = get_userdata( $member )
+				$user_data = get_userdata( $member );
+                $bpUser = new BP_Core_User($user_data->ID);
+                $bpUserProfile = $bpUser->get_profile_data();
+                //print_r($bpUserProfile);
 				?>
-				<li id="<?php echo $user_data->ID ?>"
-				    class="select2-results__option select2-results__option--highlighted" role="treeitem"
+				<li id="user-<?php echo $user_data->ID ?>"
+				    class="select2-results__option select2-results__option--highlighted tk-post-member-item" role="treeitem"
 				    aria-selected="false">
 					<div class="select2-result-user clearfix">
-						<div class="select2-result-user__avatar"><img
-								src="<?php echo get_avatar_url( $member ) ?>"></div>
+						<div class="select2-result-user__avatar">
+                            <a href="<?php echo $bpUser->user_url; ?>">
+                            <?php echo $bpUser->avatar; ?>
+                            </a>
+                        </div>
 						<div class="select2-result-user__meta">
-							<div class="select2-result-user__display_name"><?php echo $user_data->display_name ?></div>
-							<div class="select2-result-user__user_email"><?php echo $user_data->user_email ?></div>
+							<!--<div class="select2-result-user__display_name"><?php //echo $user_data->display_name ?></div>-->
+
+                            <?php
+                            $anrede = '';
+                            $fields = '';
+                            //print_r($bpUserProfile);
+                            foreach ($bpUserProfile as $key => $value) {
+                                //if(is_array($value)) echo $value['field_data'].'<br/>';
+                                //echo $key.': '.$value.'<br/>';
+                                if($key == 'Name') $username = $value['field_data'];
+                                else if($key == 'Anrede' || $key == 'Titel' || $key == 'Vorname' || $key == 'Nachname') $anrede .= $value['field_data'].' ';
+                                else if($key == 'user_login' || $key == 'user_nicename' || $key == 'user_email') continue;
+                                else $fields .= $key.': '.$value['field_data'].'<br/>';
+                                //echo '<div class="select2-result-user__display">'.$field['field_data'].'</div>';
+                            }
+                            echo '<h3>'.$anrede.'</h3>';
+                            echo 'Nutzername: <a href="'.$bpUser->user_url.'">'.$username.'</a><br/>';
+                            echo $fields;
+                            ?>
+
+                            <!--<div class="select2-result-user__user_position"><?php echo $bpUserProfile['Aufgabenbereiche']['field_data'] ?></div>
+							<div class="select2-result-user__user_email"><?php echo $user_data->user_email ?></div>-->
+
 							<div class="select2-result-user__actions">
-								<div class="select2-result-user__add"><a data-id="<?php echo $user_data->ID ?>"
-								                                         class="tk-pm-remove-member">Any Action?</a>
+								<div class="select2-result-user__add">
+                                    <!-- /nutzerverzeichnis/daadmin/messages/compose/?r=inforiesatsbaucom-2-2-2-2 -->
+                                    <a href="<?php echo $bpUser->user_url; ?>" data-id="<?php echo $user_data->ID ?>" class="tk-pm-remove-member">&gt; Profil</a>
 								</div>
 							</div>
 						</div>
 					</div>
-					<input type="hidden" value="<?php echo $user_data->ID ?>" name="_tk_post_members[]"></li>
+					<input type="hidden" value="<?php echo $user_data->ID ?>" name="_tk_post_members[]">
+                <hr/>
+                </li>
 
 				<?php
 			}
